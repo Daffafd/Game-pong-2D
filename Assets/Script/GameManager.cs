b57708525240fd42f271f352a3b4c1f421ed3cb4
Daffafd <122620156+Daffafd@users.noreply.github.com>
2023-04-03 18:29:39 +0700

    Second Commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int PlayerScoreL = 0;
+    public int PlayerScoreR = 0;
+
+    //Buat UI Text
+    public TMP_Text txtPlayerScoreL;
+    public TMP_Text txtPlayerScoreR;
+
+    public static GameManager instance;
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+  
+        txtPlayerScoreL.text = PlayerScoreL.ToString();
+        txtPlayerScoreR.text = PlayerScoreR.ToString();
+    }
+
+
+   
+    public void Score(string wallID)
+    {
+        if (wallID == "Kiri")
+        {
+            PlayerScoreR = PlayerScoreR + 1; 
+            txtPlayerScoreR.text = PlayerScoreR.ToString(); 
+        }
+        else
+        {
+            PlayerScoreL = PlayerScoreL + 1;
+            txtPlayerScoreL.text = PlayerScoreL.ToString();
+
+        }
+    }
+    public void ScoreCheck()
+    {
+        if (PlayerScoreL == 7)
+        {
+            Debug.Log("playerL win");
+            this.gameObject.SendMessage("ChangeScene", "Menu");
+        }
+        else if (PlayerScoreR == 7)
+        {
+            Debug.Log("playerR win");
+            this.gameObject.SendMessage("ChangeScene", "Menu");
+        }
+    }
+
+}
--- Assets/Script/SceneManagement.cs
+++ Assets/Script/SceneManagement.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
